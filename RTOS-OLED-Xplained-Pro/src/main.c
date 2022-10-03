20fab08c1842d405ac7a3709994385838ec81edb
Sarah <72346043+sarahp31@users.noreply.github.com>
2022-10-03 16:36:55 -0300

    task mode criada e fila mode criada e funcionando

--- RTOS-OLED-Xplained-Pro/src/main.c
+++ RTOS-OLED-Xplained-Pro/src/main.c
@@ -11,6 +11,21 @@
 #define BUT_PIO_PIN 11
 #define BUT_PIO_PIN_MASK (1 << BUT_PIO_PIN)
 
+#define BUT_1_PIO PIOD
+#define BUT_1_PIO_ID ID_PIOD
+#define BUT_1_IDX 28
+#define BUT_1_IDX_MASK (1u << BUT_1_IDX)
+
+#define BUT_2_PIO PIOC
+#define BUT_2_PIO_ID ID_PIOC
+#define BUT_2_IDX 31
+#define BUT_2_IDX_MASK (1u << BUT_2_IDX)
+
+#define BUT_3_PIO PIOA
+#define BUT_3_PIO_ID ID_PIOA
+#define BUT_3_IDX 19
+#define BUT_3_IDX_MASK (1u << BUT_3_IDX)
+
 /** RTOS  */
 #define TASK_OLED_STACK_SIZE                (1024*6/sizeof(portSTACK_TYPE))
 #define TASK_OLED_STACK_PRIORITY            (tskIDLE_PRIORITY)
@@ -23,6 +38,9 @@ extern void xPortSysTickHandler(void);
 
 /** prototypes */
 void but_callback(void);
+void but1_callback(void);
+void but2_callback(void);
+void but3_callback(void);
 static void BUT_init(void);
 
 /************************************************************************/
@@ -43,15 +61,51 @@ extern void vApplicationMallocFailedHook(void) {
 }
 
 /************************************************************************/
+/* global variables                                                     */
+/************************************************************************/
+
+QueueHandle_t xQueueMode;
+
+
+/************************************************************************/
 /* handlers / callbacks                                                 */
 /************************************************************************/
 
 void but_callback(void) {
 }
 
+void but1_callback(void) {
+	int angulo = 180;
+	xQueueSendFromISR(xQueueMode, &angulo, 0);
+}
+
+void but2_callback(void) {
+	int angulo = 90;
+	xQueueSendFromISR(xQueueMode, &angulo, 0);
+}
+
+
+void but3_callback(void) {
+	int angulo = 45;
+	xQueueSendFromISR(xQueueMode, &angulo, 0);
+}
+
+
 /************************************************************************/
 /* TASKS                                                                */
 /************************************************************************/
+
+static void task_modo(void *pvParameters){
+	
+	BUT_init();
+	
+	int angulo;
+	for(;;){
+		if( xQueueReceive(xQueueMode, &angulo, ( TickType_t ) 500 )){
+			printf("angulo = %d \n", angulo);
+		}
+	}
+}
 
 static void task_oled(void *pvParameters) {
 	gfx_mono_ssd1306_init();
@@ -93,6 +147,39 @@ static void BUT_init(void) {
 	pio_set_debounce_filter(BUT_PIO, BUT_PIO_PIN_MASK, 60);
 	pio_enable_interrupt(BUT_PIO, BUT_PIO_PIN_MASK);
 	pio_handler_set(BUT_PIO, BUT_PIO_ID, BUT_PIO_PIN_MASK, PIO_IT_FALL_EDGE , but_callback);
+	
+	pmc_enable_periph_clk(BUT_1_PIO_ID);
+	pmc_enable_periph_clk(BUT_2_PIO_ID);
+	pmc_enable_periph_clk(BUT_3_PIO_ID);
+	
+	pio_configure(BUT_1_PIO, PIO_INPUT, BUT_1_IDX_MASK, PIO_PULLUP| PIO_DEBOUNCE);
+	pio_configure(BUT_2_PIO, PIO_INPUT, BUT_2_IDX_MASK, PIO_PULLUP| PIO_DEBOUNCE);
+	pio_configure(BUT_3_PIO, PIO_INPUT, BUT_3_IDX_MASK, PIO_PULLUP| PIO_DEBOUNCE);
+	
+	pio_handler_set(BUT_1_PIO, BUT_1_PIO_ID, BUT_1_IDX_MASK, PIO_IT_FALL_EDGE,
+	but1_callback);
+	pio_handler_set(BUT_2_PIO, BUT_2_PIO_ID, BUT_2_IDX_MASK, PIO_IT_FALL_EDGE,
+	but2_callback);
+	pio_handler_set(BUT_3_PIO, BUT_3_PIO_ID, BUT_3_IDX_MASK, PIO_IT_FALL_EDGE,
+	but3_callback);
+	
+	pio_enable_interrupt(BUT_1_PIO, BUT_1_IDX_MASK);
+	pio_enable_interrupt(BUT_2_PIO, BUT_2_IDX_MASK);
+	pio_enable_interrupt(BUT_3_PIO, BUT_3_IDX_MASK);
+	
+	pio_get_interrupt_status(BUT_1_PIO);
+	pio_get_interrupt_status(BUT_2_PIO);
+	pio_get_interrupt_status(BUT_3_PIO);
+
+	NVIC_EnableIRQ(BUT_1_PIO_ID);
+	NVIC_SetPriority(BUT_1_PIO_ID, 4);
+
+	NVIC_EnableIRQ(BUT_2_PIO_ID);
+	NVIC_SetPriority(BUT_2_PIO_ID, 4);
+
+	NVIC_EnableIRQ(BUT_3_PIO_ID);
+	NVIC_SetPriority(BUT_3_PIO_ID, 4);
+	
 }
 
 /************************************************************************/
@@ -112,6 +199,19 @@ int main(void) {
 	if (xTaskCreate(task_oled, "oled", TASK_OLED_STACK_SIZE, NULL, TASK_OLED_STACK_PRIORITY, NULL) != pdPASS) {
 	  printf("Failed to create oled task\r\n");
 	}
+	
+	if (xTaskCreate(task_modo, "modo", TASK_OLED_STACK_SIZE, NULL, TASK_OLED_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create modo task\r\n");
+	}
+	
+	// cria fila de 32 slots de char
+	xQueueMode = xQueueCreate(64, sizeof(int) );
+	
+	// verifica se fila foi criada corretamente
+	if (xQueueMode == NULL){
+		printf("falha em criar a fila mode \n");
+	}
+	
 
 	/* Start the scheduler. */
 	vTaskStartScheduler();
